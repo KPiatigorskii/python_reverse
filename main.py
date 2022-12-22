f5a2bba1239bce7ab80bbcd86742542dbd6f0b11
KPiatigorskii <k.piatigorskii@gmail.com>
2022-12-22 12:07:10 +0200

    add reverse

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+import os
+print(os.path.basename(__file__)[::-1])
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
